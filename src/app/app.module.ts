da0c7f5c8402a973ff6113dcc5d6887b62f5f052
Sedikane <mogorueve@gmail.com>
2018-10-23 14:15:30 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { MediaCapture } from '@ionic-native/media-capture';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -14,7 +14,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +24,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    MediaCapture,
+    
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions, CaptureVideoOptions,CaptureAudioOptions} from '@ionic-native/media-capture';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,private mediaCapture: MediaCapture) {
   }
 
+  picture(){
+    let options: CaptureImageOptions = { limit: 3 };
+    this.mediaCapture.captureImage(options)
+      .then(
+        (data: MediaFile[]) => console.log(data),
+        (err: CaptureError) => console.error(err)
+      );
+    
+    }
+  video(){
+let options: CaptureVideoOptions = { limit: 3 };
+this.mediaCapture.captureVideo(options)
+  .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  );
+  }
+  audio(){
+  let options: CaptureAudioOptions = { limit: 3 };
+  this.mediaCapture.captureAudio(options)
+  .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  );
+  
+  }
 }
